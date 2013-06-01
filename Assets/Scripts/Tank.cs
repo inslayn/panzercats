f4280c7a9196b51036ff93156d02c18f2013cf73
David Rzepa <david.rzepa@gmail.com>
2013-06-01 16:07:27 +0100

    - Changed tank physics

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -11,17 +11,27 @@ public class Tank : MonoBehaviour {
 	
 	[SerializeField]
 	GameObject bulletPrefab;
-	
+
+	[SerializeField]
+	Transform leftTread = null, rightTread = null;
+
+	[SerializeField]
+	WheelCollider[] leftWheelColliders = null, rightWheelColliders;
+
+	[SerializeField]
+	Camera tankCamera = null;
+
 	Transform cachedTransform;
 	
-	float speed = 2f, rotationSpeed = 200f, vertical, horizontal;
+	float speed = 2f, rotationSpeed = 100f, vertical, horizontal;
 
 	//----------------------------------------------------------------------------------------
 	
 	void Start()	
 	{
 		enabled = networkView.isMine;
-		
+		tankCamera.enabled = networkView.isMine;
+
 		cachedTransform = transform;
 		
 		InvokeRepeating("CheckParticles", 0.1f, 1f);
@@ -29,22 +39,31 @@ public class Tank : MonoBehaviour {
 	
 	//----------------------------------------------------------------------------------------
 	
-	void Update () 
+	void FixedUpdate () 
 	{
 		vertical = Input.GetAxis("Vertical"); 
 		horizontal = Input.GetAxis("Horizontal");
-		
-		if(vertical != 0)
-//			cachedTransform.rigidbody.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
-        	cachedTransform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
-		
-		if(horizontal != 0)
-        	cachedTransform.Rotate(0, (horizontal * rotationSpeed) * Time.deltaTime, 0);
-		
+
+		Vector3 thrustVector = cachedTransform.forward;
+		thrustVector.y = 0f;
+
+		float forwardThrottle = vertical;
+		float leftThrottle = horizontal+vertical;
+		float rightThrottle = -horizontal+vertical;
+
+		float treadDifference = leftThrottle-rightThrottle;
+
+		rigidbody.AddTorque( 0, 0.1f*(treadDifference * rotationSpeed)/(1f+5f*rigidbody.velocity.magnitude), 0 );
+		rigidbody.AddForce( 10f*(leftThrottle+rightThrottle)*thrustVector );
+
+
+	}
+
+	void Update() {
 		if(Input.GetKeyDown(KeyCode.Space))
 			StartCoroutine(FireBullet());
 	}
-	
+
 	//----------------------------------------------------------------------------------------
 	
 	IEnumerator FireBullet()
