ab0dbe3f5a4d2e8b885e2066088823daec490818
Nick McVroom-Amoakohene <nick@nickmcvroom.com>
2013-06-02 14:45:38 +0100

    UI

--- Assets/Scripts/Panzer Cats/GameManager.cs
+++ Assets/Scripts/Panzer Cats/GameManager.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 public class GameManager : MonoSingleton<GameManager> {
 
     public enum Screens {
+        Splash,
         Title,
+        Menu,
         Lobby,
         InGame,
         Credits,
@@ -16,6 +18,9 @@ public class GameManager : MonoSingleton<GameManager> {
     [SerializeField]
     List<U9View> views;
 
+    [SerializeField]
+    GameObject background;
+
     Dictionary<Screens, System.Type> screenToTypeMap;
 
     void Awake() {
@@ -24,15 +29,30 @@ public class GameManager : MonoSingleton<GameManager> {
         stack = GetComponent<U9ViewStack>();
 
         screenToTypeMap = new Dictionary<Screens, System.Type>() {
+            { Screens.Splash, typeof(SplashScreen) },
             { Screens.Title, typeof(TitleScreen) },
+            { Screens.Menu, typeof(MenuScreen) },
             { Screens.Lobby, typeof(LobbyScreen) },
             { Screens.InGame, typeof(InGameScreen) },
             { Screens.Credits, typeof(CreditsScreen) },
         };
     }
 
-    public U9Transition Show(Screens screen) {
+    void Start() {
+        Show(Screens.Splash);
+    }
+
+    public U9Transition Show(Screens screen, bool disableBackground = false) {
         U9View view = GetViewFromType(screenToTypeMap[screen]);
+
+        if(disableBackground) {
+            if(background.activeSelf)
+                background.SetActive(false);
+        }
+        else {
+            if(!background.activeSelf)
+                background.SetActive(true);
+        }
 
         if(view)
             return stack.GetPushViewTransition(view);
@@ -40,11 +60,15 @@ public class GameManager : MonoSingleton<GameManager> {
         return U9T.Null();
     }
 
+    public void HideCurrentScreen() {
+        stack.GetPopViewTransition().Begin();
+    }
+
     U9View GetViewFromType(System.Type type) {
         U9View view = null;
 
         foreach(U9View v in views) {
-            if(views.GetType() == type) {
+            if(v.GetType() == type) {
                 view = v;
                 break;
             }
--- Assets/Scripts/Panzer Cats/Screens/LobbyScreen.cs
+++ Assets/Scripts/Panzer Cats/Screens/LobbyScreen.cs
@@ -2,4 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class LobbyScreen : U9SlideView {
+    void BackToMenu() {
+        GameManager.Instance.HideCurrentScreen();
+    }
+
+    void JoinTheFight() {
+        GameManager.Instance.Show(GameManager.Screens.InGame, true).Begin();
+    }
 }
