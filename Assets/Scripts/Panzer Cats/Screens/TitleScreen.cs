4afdd0adaf7f5b01b9a7e85406923dc1129524ff
Laurentiu Fenes <laufenes@yahoo.com>
2013-06-02 16:37:48 +0100

    ui fixes

--- Assets/Scripts/Panzer Cats/Screens/TitleScreen.cs
+++ Assets/Scripts/Panzer Cats/Screens/TitleScreen.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class TitleScreen : U9SlideView {
-    void Update() {
-        if(Input.GetKeyUp(KeyCode.Return)) {
-            GameManager.Instance.Show(GameManager.Screens.Menu).Begin();
-        }
-    }
+	protected override void EndDisplay() {
+		Invoke("Menu", 3f);
+	}
+	
+	void Menu() {
+       GameManager.Instance.Show(GameManager.Screens.Menu).Begin();
+	}
 }
